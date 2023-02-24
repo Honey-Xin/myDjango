fa2cb8f8ceaaa41ddbe4190fbaa6db2dfc02dcc9
Honey <1355885920@qq.com>
2023-02-24 17:00:27 +0800

    1

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class auThor(models.Model):
+    # author_id = models.AutoField(primary_key=True,unique=True,verbose_name="作者ID")
+    # author_id = models.IntegerField('作者ID',max_length=11,null=False)
+    auThor_name = models.CharField('姓名',max_length=255)
+    auThor_sex = models.CharField('性别',max_length=20)
+    auThor_birthy = models.CharField('出生年月',max_length=255,null=True)
+    auThor_phone = models.IntegerField('联系方式',max_length=11,null=True)
+
+    user = models.ForeignKey(to=User,on_delete=models.CASCADE,null=True)
+
+
+class arTicle(models.Model):
+    # arTicle_id = models.AutoField(primary_key=True,unique=True,verbose_name='剧本ID')
+    # arTicle_id = models.IntegerField('剧本ID',max_length=11,null=False)
+    arTicle_name = models.CharField('剧本名',max_length= 255)
+    arTicle_theme = models.CharField('剧本主题',max_length=255)
+    arTicle_content = models.CharField('剧本内容',max_length=10000,null=False)
+    arTicle_score = models.IntegerField('剧本得分',max_length=11)
+
+    arTicle_auThor_id = models.ForeignKey(to=auThor,on_delete=models.CASCADE,null=True) # 关联
---
+++ app/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.views import View
+from django.contrib.auth.models import User
+from app.models import *
+# Create your views here.
+
+
+def get_index(request):
+    return render(request,'index.html')
+
+def to(request):
+    return render(request,'test.html')
+
+# 类视图---登录模板
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html")
+
+    def post(self, request):
+        lname = request.POST.get("lname")
+        lpasswd = request.POST.get("lpassword")
+
+        # 登录失败时需要提示是用户名不存在还是密码错误
+        try:  # 存放可能出现异常的代码 查询数据多个条件时默认是并且的关系
+            user = User.objects.get(username=lname)
+            # 当输入的用户名在数据库里查询不到，说明try里面的代码存在异常
+            # 执行万能异常里面的语句
+        except Exception as e:  # 捕获异常将异常存到e里
+            print(e)
+            return HttpResponse("用户名不存在")
+
+        # 如果用户名对，就判断密码有没有输入正确
+        if lpasswd != user.password:
+            return HttpResponse("用户名和密码不匹配")
+
+        return render(request,'main.html',{"username":user})
+
+class RegisterView(View):
+    def get(self,request):
+        return render(request, 'register.html')
+
+    def post(self, request):
+        rname = request.POST.get("email")
+        rpasswd = request.POST.get("pwd1")
+
+        # 注册成功跳转到到登录页面，注册加判断已经存在提示改用用户已存在
+        users = User.objects.all()
+        for i in users:
+            if rname == i.username:
+                return HttpResponse("用户已存在")
+        try:
+            User.objects.create(username=rname, password=rpasswd)
+        except Exception as e:
+            print(e)
+            return HttpResponse("注册失败")
+        return redirect('logined')
+
+from app.cope import goON
+#处理模块
+def DoModify(request):
+    goON.
